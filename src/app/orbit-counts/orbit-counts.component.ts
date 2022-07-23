754e3a4e6effb1ab77b1bd6d96f4fd311882b6ae
Peggy Gillette <dark_aoekae@yahoo.com>
2022-07-22 21:33:48 -0500

    added ngIf statement to html file, and new totalCount function

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -16,16 +16,65 @@ export class OrbitCountsComponent implements OnInit {
   }
 
   countByType(type: string): number {
+
 	let count = 0;
 	if (this.satellites) {
-	  for (let i = 0; i < this.satellites.length; i++) {
-		 if (this.satellites[i].type === type) {
+	  for (let index = 0; index < this.satellites.length; index++) {
+		 if (this.satellites[index].type === type) {
 			count++;
 		 }
-	  }
+	  } 
 	}
 	return count;
  }
 
+totalCount (type: string): number[] {
+	let totalCountArray: number[]= [];
+	let total: number = 0;
+	let communication: number = 0;
+	let probe: number = 0;
+	let positioning: number = 0;
+	let spaceDebris: number = 0;
+	let spaceStation: number = 0;
+	let telescope: number = 0;
+	
+	 if (this.satellites) {
+		for(let index = 0; index < this.satellites.length; index++) {
+			if (this.satellites[index].type === "Communication") {
+				communication++
+			}
+			if (this.satellites[index].type === "Probe") {
+				probe++
+			}
+			if (this.satellites[index].type === "Positioning") {
+				positioning++
+			}
+			if (this.satellites[index].type === "Space Debris") {
+				spaceDebris++
+			}
+			if (this.satellites[index].type === "Space Station") {
+				spaceStation++
+			}
+			if (this.satellites[index].type === "Telescope") {
+				telescope++
+			}
+		}
+		total = communication + probe + positioning + spaceDebris + spaceStation + telescope;
+		totalCountArray.push(communication, probe, positioning, spaceDebris, spaceStation, telescope, total)
+	 }
+console.log(totalCountArray)
+return totalCountArray;
+
+} 
+// 4) Counting Satellites
+// orbit-counts is a component that shows the total number of satellites currently displayed in the table. Or at least, that's what it should do. Right now, it's just displaying NaN.
+
+// To fix this, you'll need to replace the NaN string with a structural directive to indicate how many satellites are on display in the table. Like orbit-list, the orbit-counts component has access to the array of fetched satellites using an input property called satellites.
+
+// The desired functionality is that the number displayed with change with the number of satellites displayed. Checkout this gif for reference. Note, you do not need to display the counts for each satellite type to pass the autograder, just use the satellite property to show the total number of satellites displayed.
+
+// Bonus Missions
+// 1) Zebra Stripes
+// Alternate the color for every other row in the table. Choose whichever pair of colors you prefer, but the highlighting for space debris should still be distinct.
 
 }
--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -24,14 +24,3 @@ export class Satellite {
 } 
 
 }
-
-// TODO 3a: fix isSpaceDebris check
-
-
-// You need to make it easier to spot dangerous space debris in the satellite data. Inside of the Satellite class, there is a broken method called isSpaceDebris. This is meant to return true only when a Satellite object has this type.
-
-// To highlight space debris, you first need to fix this method. Next, use the method to add a warning class onto the table cell. There's already a css style rule to make any element marked with the warning class appear red.
-
-// Repair the isSpaceDebris method in the Satellite class. isSpaceDebris returns true if the satellite type is 'Space Debris', and it returns false otherwise.
-
-// Use isSpaceDebris to add the warning CSS class to the <td> containing the satellite's type. Use an Angular attribute directive to accomplish this.
