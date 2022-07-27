f13b391c57d0918f7babbdc10eb6ba6a947a2825
Peggy Gillette <dark_aoekae@yahoo.com>
2022-07-26 20:41:32 -0500

    Fixed some stuff Ted told me about

--- src/app/orbit-counts/orbit-counts.component.spec.ts
+++ src/app/orbit-counts/orbit-counts.component.spec.ts
@@ -34,17 +34,17 @@ describe('OrbitCountsComponent', () => {
   it('conditionally counts how many satellites are displayed', () => {
 	component.satellites = [new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true)];
 	fixture.detectChanges();
-	let counts = element.query(By.css('.counts-total'));
+	let counts = element.query(By.css('.counts'));
 	expect(counts.nativeElement.innerHTML).toBe('1');
 
 	component.satellites = [new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true), new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true)];
 	fixture.detectChanges();
-	counts = element.query(By.css('.counts-total'));
+	counts = element.query(By.css('.counts'));
 	expect(counts.nativeElement.innerHTML).toBe('2');
 
 	component.satellites = [];
 	fixture.detectChanges();
-	counts = element.query(By.css('.counts-total'));
+	counts = element.query(By.css('.counts'));
 	expect(counts.nativeElement.innerHTML).toBe('0');
 	
 
